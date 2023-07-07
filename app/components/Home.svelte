e708f083b255248185a29198344aafae5e091b1d
sacha <sacha@sachamasry.com>
2023-07-07 01:19:56 +0000

    Updated hour text chaining, correcting various display problems.
    Created a one-second timeout for updating the time continuously.
    
    Added the minutes display, in addition to the minute blocks.
    
    Functions now update variables and variables are printed on screen.
    
    FossilOrigin-Name: 036ec3b0e14abd93fcf5d5decb6d988c6015c69811d1562a6b3355ecccb202e8

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -3,9 +3,11 @@
         <label class="info">
             <formattedString>
                 <span text="{message}" />
-                <span text=" {get_minutes_string(current_time)}" />
-                <span text=" {time_joiner_string(current_time)}" />
-                <span text=" {get_hour_string(current_time)}" />
+                <span text=" {minute_block_string}" />
+                <span text=" {time_joiner_string}" />
+                <span text=" {hour_string}" />
+                <span text=" {hour_terminator_string}" />
+                <span text=" {minutes_display}" />
             </formattedString>
         </label>
 
@@ -24,13 +26,28 @@
 <script lang="ts">
 let hours_in_words = ["twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
 
-let minute_block_in_words = ["o'clock", "five", "ten", "quarter", "twenty", "twenty-five", "half", "twenty-five", "quarter", "ten", "five"];
+let minute_block_in_words = ["", "five", "ten", "quarter", "twenty", "twenty-five", "half", "twenty-five", "twenty", "quarter", "ten", "five"];
 
-let hour_prefix = "";
+let current_time = new Date();
 
-let hour_suffix = "";
+let message: string = "it is"
+let minute_block_string = get_minute_block_string(current_time);
+let minutes_display = get_minutes_string(current_time);
+let time_joiner_string = get_time_joiner_string(current_time);
+let hour_string = get_hour_string(current_time);
+let hour_terminator_string = get_on_the_hour(current_time);
+let seconds_string = current_time.getSeconds().toString();
 
-let current_time = new Date;
+var x = setInterval(() => {
+  current_time = new Date();
+  minute_block_string = get_minute_block_string(current_time);
+  minutes_display = get_minutes_string(current_time);
+  time_joiner_string = get_time_joiner_string(current_time);
+  hour_string = get_hour_string(current_time);
+  hour_terminator_string = get_on_the_hour(current_time);
+  seconds_string = current_time.getSeconds().toString();
+}, 1000)
+
 
 function get_hour_string(date) {
   let hour = date.getHours() + get_hour_adder(date);
@@ -70,20 +87,35 @@ function get_hour_adder(date) {
   }
 }
 
-function get_minutes_string(date) {
+function get_minute_block_string(date) {
   return minute_block_in_words[get_minutes_breakdown(date).block];
 }
 
-function time_joiner_string(date) {
-  if (get_minutes_breakdown(date).block > 6) {
-    return "to"
-  } else {
+function get_minutes_string(date) {
+  let minutes_display = "";
+  for (i = 1; i <= get_minutes_breakdown(current_time).minutes; i += 1) {
+    minutes_display = minutes_display + "· "
+  }
+  return minutes_display;
+}
+
+function get_time_joiner_string(date) {
+  if (get_minutes_breakdown(date).block === 0) {
+    return ""
+  } else if (get_minutes_breakdown(date).block <= 6){
     return "past"
+  } else {
+    return "to"
   }
 }
 
-    let message: string = "it is"
-let hour_string = {get_hour_string(current_time)}
+function get_on_the_hour(date) {
+  if (get_minutes_breakdown(date).block === 0) {
+    return "o'clock"
+  } else {
+    return ""
+  }
+}
 </script>
 
 <style>
